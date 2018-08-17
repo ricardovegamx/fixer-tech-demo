5ef6de19fe84781917862c9674cc2ff9bcee333c
richardmx <dev.ricardo.vega@gmail.com>
2018-08-17 10:59:25 -0500

    added config files for main App component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,141 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="border-t-4 border-indigo-dark">
+    <div class="container mt-8">
+      <heading></heading>
+      <div class="flex items-center justify-center w-full my-4">
+        <flat-pickr 
+          v-model="date"
+          :config="flatPickrConfig"
+          class="border-0 lg:border-2 lg:border-grey lg:border-r-0 rounded-l px-4 py-2"
+          placeholder="Select a date to start"></flat-pickr>
+        <button 
+          class="border-1 bg-indigo-dark lg:border-2 border-indigo-dark text-white font-bold rounded-r px-4 py-2" 
+          @click="update">Refresh Rates</button>
+      </div>
+      <div 
+        v-if="!isLoading"
+        id="chart" 
+        class="h-128 w-full"></div>
+      <div v-else>
+        <p class="text-grey-darker text-sm text-center">Fetching Information…</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import './assets/css/app.css'
-import HelloWorld from './components/HelloWorld.vue'
+import { apiKey, baseUrl, sampleCurrencies, baseCurrency, FixerApiConstraints } from "./assets/js/config.js";
+import Heading from "./components/Heading.vue";
+import * as moment from "moment-mini";
+import axios from "axios";
+import "flatpickr/dist/flatpickr.css";
+import flatPickr from "vue-flatpickr-component";
+import "../node_modules/c3/c3.min.css";
+import c3 from "c3";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    Heading,
+    flatPickr,
+  },
+  data() {
+    return {
+      date: moment().format("YYYY-MM-DD"),
+      flatPickrConfig: {
+        altFormat: "F j, Y",
+        altInput: true,
+        dateFormat: "Y-m-d",
+        defaultDate: "today",
+        minDate: FixerApiConstraints.minDate,
+        maxDate: FixerApiConstraints.maxDate,
+      },
+      currencies: "",
+      valuesInMXN: "",
+      isLoading: false,
+    };
+  },
+  computed: {
+    apiUrl() {
+      return baseUrl + this.date + "?access_key=" + apiKey + "&symbols=" + sampleCurrencies.toString();
+    },
+  },
+  mounted() {
+    this.loadFromLocalStorage();
+  },
+  methods: {
+    update() {
+      this.isLoading = true;
+      if (!this.loadFromLocalStorage()) {
+        axios
+          .get(this.apiUrl)
+          .then(response => {
+            if (response.status == 200 && response.data.success) {
+              if (response.data.hasOwnProperty("rates")) {
+                this.setLocalStorage(this.date, JSON.stringify(response.data.rates));
+                this.convertValuesToMXN(response.data.rates);
+                this.isLoading = false;
+                this.renderChart();
+              }
+            }
+          })
+          .catch(error => {
+            if (error.message) {
+              alert("Cannot establish a proper connection with Fixer API");
+            }
+          });
+      }
+    },
+    loadFromLocalStorage() {
+      if (this.getLocalStorage(this.date)) {
+        let chartData = JSON.parse(this.getLocalStorage(this.date));
+        this.convertValuesToMXN(chartData);
+        this.isLoading = false;
+        this.renderChart();
+      } else {
+        return false; 
+      }
+    },
+    convertValuesToMXN(rates) {
+      this.currencies = Object.keys(rates);
+      let valuesInMXN = [];
+      Object.values(rates).forEach((rate, index) => valuesInMXN[index] = rates.MXN / rate);
+      valuesInMXN.unshift("Values in Mexican Peso");
+      this.valuesInMXN = valuesInMXN;
+      this.removeMXNFromResults();
+    },
+    removeMXNFromResults() {
+      let indexOfMXN = this.currencies.indexOf(currency => currency == baseCurrency);
+      this.currencies.splice(indexOfMXN, 1);
+      this.valuesInMXN.splice(indexOfMXN, 1);
+    },
+    getLocalStorage(key) {
+      return window.localStorage.getItem(key);
+    },
+    setLocalStorage(key, value) {
+      window.localStorage.setItem(key, value);
+    },
+    renderChart() {
+      this.$nextTick(() => {
+        c3.generate({
+          data: {
+            columns: [ this.valuesInMXN ]
+          },
+          axis: {
+            x: {
+              type: "category",
+              categories: this.currencies,
+              tick: {
+                culling: {
+                  max:1
+                }
+              }
+            }
+          }
+        });
+      });
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/css/app.css"></style>
---
+++ src/assets/js/config.js
@@ -0,0 +1,25 @@
+export const apiKey = "6cf523520a968d30d99500a2774c5a17";
+export const baseUrl = "http://data.fixer.io/api/";
+export const baseCurrency = "MXN";
+export const FixerApiConstraints = {
+  minDate: "1999-01-01",
+  maxDate: "today"
+};
+
+export var sampleCurrencies = [
+  "NZD",
+  "LYD",
+  "SGD",
+  "CAD",
+  "AUD",
+  "USD",
+  "CHF",
+  "EUR",
+  "KYD",
+  "GBP",
+  "JOD",
+  "OMR",
+  "BHD",
+  "KWD",
+  "MXN"
+];
