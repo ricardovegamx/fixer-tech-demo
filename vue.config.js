be88ef6ca1a1c33e65408c16871006926bc8a88a
richardmx <dev.ricardo.vega@gmail.com>
2018-08-17 11:01:06 -0500

    cleaned up file

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,6 @@ module.exports = {
     optimization: {
       splitChunks: false
     },
-  },
-
-  baseUrl: undefined,
-  outputDir: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
+  },  
   productionSourceMap: false,
-  parallel: undefined,
-  css: undefined
 };
