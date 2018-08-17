83e895e3f66cda40412325df4875659bf8bcc570
richardmx <dev.ricardo.vega@gmail.com>
2018-08-17 11:00:29 -0500

    moved heading into its own component

---
+++ src/components/Heading.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="text-center text-indigo-dark text-3xl uppercase mb-2">Mexican Peso Historical FX Rates</h1>
+    <p class="text-center text-grey-darkest">Compare Mexican Peso against World's Top Currencies in a selected date.</p>
+  </div>
+</template>
