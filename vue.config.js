5272d83b719e51769d77962aea4e58d173232083
richardmx <dev.ricardo.vega@gmail.com>
2018-08-17 11:00:05 -0500

    removed source maps from production

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    configureWebpack: {
-        // Disable splitChunks to get a single minified file
-        optimization: {
-            splitChunks: false
-        },
-    }
+  configureWebpack: {
+    // Disable splitChunks to get a single minified file
+    optimization: {
+      splitChunks: false
+    },
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
 };
